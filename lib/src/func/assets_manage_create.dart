60c616bbfc7bec4ef74b4d21bd50f2f83e110053
shuangjian.zhou <1542100658@qq.com>
2022-02-10 18:42:01 +0800

    基础功能搭建完成

--- lib/src/func/assets_manage_create.dart
+++ lib/src/func/assets_manage_create.dart
@@ -20,39 +20,39 @@ class AssetsManageCreate {
     final linesTF = assetsTFFile.readAsLinesSync();
     for (var itemDF in linesTD) {
       assetsMFile.writeAsStringSync('\t/// $itemDF\n', mode: FileMode.append);
-      // for (var itemTF in linesTF) {
-      //   if (itemTF.contains(itemDF)) {
-      //     final last = itemTF.split(itemDF).last;
-      //     if (!last.contains('/')) {
-      //       var name = last.split('.').first;
-      //       final isReg = assetsNameRegExp.hasMatch(name);
-      //       final rAPath = AssetsTool.getRAssetsPath(itemTF);
-      //       if (isReg) {
-      //         if (name.contains('_')) {
-      //           name = name.replaceAllMapped(RegExp(r'_[a-z]'), (match) {
-      //             var char = match.group(0)!;
-      //             char = char.split('_').last;
-      //             return char.toUpperCase();
-      //           });
-      //         }
-      //         // Check if the resource has the same name.
-      //         final amContent = assetsMFile.readAsStringSync();
-      //         final rule = 'static String $name =';
-      //         if (RegExp(rule).hasMatch(amContent)) {
-      //           final line = AssetsTool.getLineFrom(path.assetsManagePath, rule);
-      //           final sPath = line.split('\'')[1];
-      //           final errorInfo = 'REASON: Resource file names are duplicated.\n  NAME: $name\n  PATH: $path\n SPATH: $sPath';
-      //           assetsNURFile.writeAsStringSync('$errorInfo\n', mode: FileMode.append);
-      //         } else {
-      //           assetsMFile.writeAsStringSync('\tstatic String $name = \'$rAPath\';\n');
-      //         }
-      //       } else {
-      //         final errorInfo = 'REASON: Resource file naming is not standard.\n  NAME: $name\n  PATH: $rAPath';
-      //         assetsNURFile.writeAsStringSync('$errorInfo\n', mode: FileMode.append);
-      //       }
-      //     }
-      //   }
-      // }
+      for (var itemTF in linesTF) {
+        if (itemTF.contains(itemDF)) {
+          final last = itemTF.split(itemDF).last;
+          if (!last.contains('/')) {
+            var name = last.split('.').first;
+            final isReg = assetsNameRegExp.hasMatch(name);
+            final rAPath = AssetsTool.getRAssetsPath(itemTF);
+            if (isReg) {
+              if (name.contains('_')) {
+                name = name.replaceAllMapped(RegExp(r'_[a-z]'), (match) {
+                  var char = match.group(0)!;
+                  char = char.split('_').last;
+                  return char.toUpperCase();
+                });
+              }
+              // Check if the resource has the same name.
+              // final amContent = assetsMFile.readAsStringSync();
+              // final rule = 'static String $name =';
+              // if (RegExp(rule).hasMatch(amContent)) {
+              //   final line = AssetsTool.getLineFrom(path.assetsManagePath, rule);
+              //   final sPath = line.split('\'')[1];
+              //   final errorInfo = 'REASON: Resource file names are duplicated.\n  NAME: $name\n  PATH: $path\n SPATH: $sPath';
+              //   assetsNURFile.writeAsStringSync('$errorInfo\n', mode: FileMode.append);
+              // } else {
+              //   assetsMFile.writeAsStringSync('\tstatic String $name = \'$rAPath\';\n');
+              // }
+            } else {
+              final errorInfo = 'REASON: Resource file naming is not standard.\n  NAME: $name\n  PATH: $rAPath';
+              assetsNURFile.writeAsStringSync('$errorInfo\n', mode: FileMode.append);
+            }
+          }
+        }
+      }
       assetsMFile.writeAsStringSync('}', mode: FileMode.append);
     }
   }
