08d7c6306a91aab41247d7749dd1b2f258de172a
shuangjian.zhou <1542100658@qq.com>
2022-02-11 18:22:58 +0800

    添加日志输出

--- lib/src/func/assets_manage_create.dart
+++ lib/src/func/assets_manage_create.dart
@@ -41,15 +41,13 @@ class AssetsManageCreate {
                 final line = AssetsTool.getLineFrom(path.assetsManagePath, rule);
                 final sPath = line.split('\'')[1];
                 final errorInfo = 'REASON: Resource file names are duplicated.\n  NAME: $last\n  PATH: $itemTF\n SPATH: $sPath';
-                assetsNURFile.writeAsStringSync('$errorInfo', mode: FileMode.append);
-                assetsNURFile.writeAsStringSync('\n ', mode: FileMode.append);
+                assetsNURFile.writeAsStringSync('$errorInfo\n\n', mode: FileMode.append);
               } else {
                 assetsMFile.writeAsStringSync('\tstatic String $name = \'$itemTF\';\n', mode: FileMode.append);
               }
             } else {
               final errorInfo = 'REASON: Resource file naming is not standard.\n  NAME: $last\n  PATH: $itemTF';
-              assetsNURFile.writeAsStringSync('$errorInfo', mode: FileMode.append);
-              assetsNURFile.writeAsStringSync('\n ', mode: FileMode.append);
+              assetsNURFile.writeAsStringSync('$errorInfo\n\n', mode: FileMode.append);
             }
           }
         }
