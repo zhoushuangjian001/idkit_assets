45992c56a479200773b600c8d640d179492b9d1d
shuangjian.zhou <1542100658@qq.com>
2022-03-29 19:10:52 +0800

    处理Window平台的路径反斜杠问题

--- test/idkit_assets_test.dart
+++ test/idkit_assets_test.dart
@@ -3,9 +3,6 @@ import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    var va = 'assets\\a\\b.txt';
-    va = va.replaceAll('\\', '/');
-    print(va);
     help();
   });
 }
